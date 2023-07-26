6ac2772f17a1c0177ced23f387f3ac8d54232fdf
Maverik010 <szymon.siwak@outlook.com>
2023-07-26 12:49:36 +0200

    first commit

---
+++ Get-Pogoda.psm1
@@ -0,0 +1,20 @@
+﻿<#
+
+.EXAMPLE
+Get-Pogoda -city Wrocław -country Poland
+
+.NOTES
+Wykonuje zapytanie webrequest.
+
+#>
+
+function Get-Pogoda([string][Parameter(mandatory=$true)]$city, [string][Parameter(mandatory=$true)]$country){
+
+    function dejMjetoPogode([string]$a,[string]$b) {
+    $invoke = (Invoke-WebRequest http://wttr.in/"$a,$b" -UserAgent "curl" )
+    # $invoke = (curl http://wttr.in/"$a,$b" -UserAgent "curl" ).Content
+    return $invoke.Content
+    }
+    
+    return dejMjetoPogode($city, $country);
+}
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Folder wkleić do jednej z 3 lokalizacji, upewnić się, że jest możliwość wykonywania skryptów nie podpisanych "Bypass"
+
+
+	C:\Users\YOUR USERNAME\Documents\WindowsPowerShell\Modules
+	C:\Program Files\WindowsPowerShell\Modules
+	C:\Windows\system32\WindowsPowerShell\v1.0\Modules
+
+
+Pozdrawiam,
+Szymon :)
