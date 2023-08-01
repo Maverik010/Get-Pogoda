61ccf3af3e77079e18e3ee296e70afab1750b2c6
maverik010 <szymon.siwak@outlook.com>
2023-08-01 16:29:50 +0200

    Zmiana kilku plików oraz przypadku z brakiem argumentów, usunięcie mandatory parameters

--- Get-Pogoda.psm1
+++ Get-Pogoda.psm1
@@ -1,22 +1,30 @@
 ﻿<#
 
 .DESCRIPTION
-Skrypt wykonujący zapytanie webrequest na serwer w celu pobrania aktualnej pogody.
+Skrypt wykonujący zapytanie webrequest na serwer w celu pobrania aktualnej pogody na okres 3 dni.
 
 .EXAMPLE
 Get-Pogoda -city Wrocław -country Poland
 
+.EXAMPLE
+Get-Pogoda -city "New York" -country "United States"
+
 .NOTES
-Wykonuje zapytanie webrequest.
+Wykonuje zapytanie webrequest. przy pomocy "curl"
 
 #>
 
-function Get-Pogoda([string][Parameter(mandatory=$true)]$city, [string][Parameter(mandatory=$true)]$country){
+function Get-Pogoda($city, $country){
 
     function dejMjetoPogode([string]$a,[string]$b) {
     $invoke = (Invoke-WebRequest http://wttr.in/"$a,$b" -UserAgent "curl" )
     return $invoke.Content
     }
+
+    if(($city -eq $null) -or ($country -eq $null)){
+        return dejMjetoPogode("Ostrów Wielkopolski", "Poland")
+    } else {
+        return dejMjetoPogode($city, $country);
+    }
     
-    return dejMjetoPogode($city, $country);
 }
